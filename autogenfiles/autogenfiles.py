12670a828abcc2ec3166a015ff9f967dde4043ad
Jessica Temporal <jessicatemporal@gmail.com>
2021-07-24 14:06:17 -0300

    Moves autogenfiles to module and creates setup.py

---
+++ autogenfiles/autogenfiles.py
@@ -0,0 +1,42 @@
+from configparser import ConfigParser
+from pathlib import Path
+
+import click
+
+from .files_helper import files
+
+
+def variables_to_substitute(variable_list):
+    parser = ConfigParser()
+    parser.read(variable_list)
+    try:
+        variables = dict(parser['variables'])
+    except KeyError:
+        print(f'Oh no! No "variables" section found in {variable_list}')
+        return
+    return variables
+
+
+@click.command()
+@click.option('-v', '--variable-list', default='variables.yaml',
+              help='Name of the yaml files containing varibales')
+@click.option('-o', '--output-path', default='./',
+              help='Output path, by default writes to root')
+def main(variable_list, output_path):
+
+    # read variables to substitute
+    if not Path(variable_list).is_file():
+        print(f'Oh no! No file named {variable_list} found!')
+        return
+
+    variables = variables_to_substitute(variable_list)
+    if not variables:
+        return
+    click.echo(f'Variables found: {list(variables.keys())}')
+
+    files(variables, output_path)
+    click.echo('Output fully written')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+REPO_URL = 'http://github.com/jtemporal/autogenfiles'
+
+with open('README.md') as fobj:
+    long_description = fobj.read()
+
+setup(
+    author='Jessica Temporal',
+    author_email='hello@jtemporal.com',
+    description='Auto generate files based on a template',
+    zip_safe=False,
+    install_requires=[
+        'click==8.0.1',
+        'Jinja2==3.0.1'
+    ],
+    keywords='auto generate files, jinja, templates, cli',
+    license='MIT',
+    long_description=long_description,
+    name='autogenfiles',
+    packages=[
+        'autogenfiles'
+    ],
+    entry_points={
+        'console_scripts': [
+            'autogenfiles = autogenfiles.autogenfiles:main',
+        ],
+    },
+    url=REPO_URL,
+    python_requires='>=3.8.2',
+    version='0.0.1',
+)
